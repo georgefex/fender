d5176d6e08381ff01c1d7f617c220325c7c42713
root <georgefex@gmail.com>
2017-05-17 05:15:12 +0000

    se agrego fabric

--- fender/settings.py
+++ fender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vdj)7+xcg4b(5z4qry_ov8=tbrc3tq^tmuj+#+utpeh*kj3w1q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.17.0.3']
 
 
 # Application definition
